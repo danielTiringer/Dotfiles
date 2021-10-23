3cf7b3fed4eda39410dfcc39ff12641917fbc49d
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-10-23 10:05:33 +0200

    Add double quotes around variables

--- zsh/.config/zsh/aliasfunctions.sh
+++ zsh/.config/zsh/aliasfunctions.sh
@@ -3,20 +3,20 @@
 extract () {
   if [ -f "$1" ] ; then
     case $1 in
-      *.tar.bz2)   tar xjf $1   ;;
-      *.tar.gz)    tar xzf $1   ;;
-      *.bz2)       bunzip2 $1   ;;
-      *.rar)       unrar x $1   ;;
-      *.gz)        gunzip $1    ;;
-      *.tar)       tar xf $1    ;;
-      *.tbz2)      tar xjf $1   ;;
-      *.tgz)       tar xzf $1   ;;
-      *.zip)       unzip $1     ;;
-      *.Z)         uncompress $1;;
-      *.7z)        7z x $1      ;;
-      *.deb)       ar x $1      ;;
-      *.tar.xz)    tar xf $1    ;;
-      *.tar.zst)   unzstd $1    ;;
+      *.tar.bz2)   tar xjf "$1"   ;;
+      *.tar.gz)    tar xzf "$1"   ;;
+      *.bz2)       bunzip2 "$1"   ;;
+      *.rar)       unrar x "$1"   ;;
+      *.gz)        gunzip "$1"    ;;
+      *.tar)       tar xf "$1"    ;;
+      *.tbz2)      tar xjf "$1"   ;;
+      *.tgz)       tar xzf "$1"   ;;
+      *.zip)       unzip "$1"     ;;
+      *.Z)         uncompress "$1";;
+      *.7z)        7z x "$1"      ;;
+      *.deb)       ar x "$1"      ;;
+      *.tar.xz)    tar xf "$1"    ;;
+      *.tar.zst)   unzstd "$1"    ;;
       *)           echo "'$1' cannot be extracted via the extract function." ;;
     esac
   else
@@ -76,9 +76,9 @@ docker_compose_update () {
         SYSTEM_ARCH=$(uname -m)
         COMPOSE_LOCATION=/usr/local/bin/docker-compose
         sudo rm $COMPOSE_LOCATION
-        sudo curl -L --fail https://github.com/docker/compose/releases/download/${LATEST_COMPOSE_VERSION}/docker-compose-${SYSTEM_TYPE}-${SYSTEM_ARCH} -o $COMPOSE_LOCATION
+        sudo curl -L --fail "https://github.com/docker/compose/releases/download/${LATEST_COMPOSE_VERSION}/docker-compose-${SYSTEM_TYPE}-${SYSTEM_ARCH}" -o $COMPOSE_LOCATION
         sudo chmod +x $COMPOSE_LOCATION
-        sudo docker pull docker/compose:$LATEST_COMPOSE_VERSION
+        sudo docker pull docker/compose:"$LATEST_COMPOSE_VERSION"
         echo "Docker-compose is upgraded, the new version is: $LATEST_COMPOSE_VERSION"
     fi
   fi
