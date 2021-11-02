c446b24efd1a35f5d280c811a38ff88c0ac1d89f
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-11-02 15:09:30 +0100

    Send user.js to its final destination(s)

--- init/common/firefox.sh
+++ init/common/firefox.sh
@@ -1,3 +1,7 @@
 # Get settings file for firefox
 
-curl --location https://raw.githubusercontent.com/ghacksuserjs/ghacks-user.js/master/user.js --output ~/Downloads/user.js
+FOLDERS=(`find ~/.mozilla/firefox/* -maxdepth 1 -type d -name "*.default*"`)
+
+for FOLDER in "${FOLDERS[@]}" ; do
+    curl --location https://raw.githubusercontent.com/ghacksuserjs/ghacks-user.js/master/user.js --output "${FOLDER}/user.js"
+done
