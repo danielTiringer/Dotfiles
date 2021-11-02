932a529009881a1d9de5a4f057884533b0668a5a
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-11-02 12:36:11 +0100

    Correction, add filename

--- init/common/firefox.sh
+++ init/common/firefox.sh
@@ -1,3 +1,3 @@
 # Get settings file for firefox
 
-curl --location https://raw.githubusercontent.com/ghacksuserjs/ghacks-user.js/master/user.js --output ~/Downloads
+curl --location https://raw.githubusercontent.com/ghacksuserjs/ghacks-user.js/master/user.js --output ~/Downloads/user.js
