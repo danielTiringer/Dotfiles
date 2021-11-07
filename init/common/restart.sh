191ceab7d6d74a154ecced4be17433544868ecf3
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-11-07 08:42:41 +0100

    Play with the restart script

--- init/common/restart.sh
+++ init/common/restart.sh
@@ -5,8 +5,16 @@ do
     echo "Installation completed."
     read -r -p 'Would you like to restart your computer now? [Y/n] ' input
     case $input in
-        [Yy][Ee][Ss]|[Yy]) echo 'Restarting in a few seconds...'; sleep 3; systemctl reboot;;
+        [Yy][Ee][Ss]|[Yy]) echo 'Restarting in a few seconds...'; sleep 3; restart;;
         [Nn][Oo]|[Nn]) echo 'No restart at this time.'; exit 1;;
         * ) echo 'Please answer yes or no.';;
     esac
 done
+
+restart () {
+    if [ $DISTRO == 'alpine' ] ; then
+        sudo reboot
+    else
+        systemctl reboot
+    fi
+}
