58dc8664f12a2a50c154c0093c12271c41ac5911
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-11-08 21:59:54 +0100

    Add wallpaper

--- init/common/wallpaper.sh
+++ init/common/wallpaper.sh
@@ -9,3 +9,4 @@ curl --location https://cdn.wallpapersafari.com/29/49/hN4mc2.jpg --output ~/Pict
 curl --location https://www.tokkoro.com/picsup/2982099-dark-debian-lenovo-blue___mixed-wallpapers.jpg --output ~/Pictures/debianLenovo.jpg
 curl --location https://www.wallpapermaiden.com/wallpaper/1432/download/1920x1080/linux-cli-commands.jpg --output ~/Pictures/commandLine.jpg
 curl --location http://getwallpapers.com/wallpaper/full/0/5/b/633941.jpg --output ~/Pictures/sundown.jpg
+curl --location https://upload.wikimedia.org/wikipedia/commons/f/fd/Sz%C3%A9chenyi_Chain_Bridge_in_Budapest_at_night.jpg?download --output ~/Pictures/lanchid.jpg
