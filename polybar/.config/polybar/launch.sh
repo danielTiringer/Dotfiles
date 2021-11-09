96f5cff7e01cbfc898ad59fe634e5d9e4c7bd3cb
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-11-09 23:26:01 +0100

    Remove non-working modules, correct which bar loads

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -12,7 +12,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # polybar -c ~/.config/polybar/config mainbar-herbst &
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-i3 &
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-herbst &
 done
 
 echo "Bars launched..."
