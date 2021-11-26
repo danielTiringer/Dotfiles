a4a7dc2c52055e545445db63dd9715d8e09d6394
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-11-26 11:50:41 +0100

    Updates to the polybar config

--- polybar/.config/polybar/scripts/pavolume.sh
+++ polybar/.config/polybar/scripts/pavolume.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # finds the active sink for pulse audio and increments the volume. useful when you have multiple audio outputs and have a key bound to vol-up and down
 
