8652143e8784fca23ad0c9228a6a65000fdb85d4
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-11-30 22:15:15 +0100

    Update eth interface handling

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 ## Add this to your wm startup file.
 
@@ -8,11 +8,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Collect data used by the bar and set them into variables
+WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}')
+WIRED=$(ls /sys/class/net/ | grep ^en | awk 'NR==1{print $1}')
+INTERFACE=$WIRED
+tail -n+3 /proc/net/wireless | grep -q . && $INTERFACE=$WIRELESS
+
 # Launch bar1 and bar2
-# polybar -c ~/.config/polybar/config mainbar-herbst &
-
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-herbst &
+	INTERFACE_NAME=$INTERFACE WIRELESS_INTERFACE=$WIRELESS WIRED_INTERFACE=$WIRED MONITOR=$m polybar --reload mainbar-herbst &
 done
-
-echo "Bars launched..."
