039d654953d646f16a504c4865021c896c766efc
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-12-01 12:31:26 +0100

    Configure the headphone sound

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,6 +9,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Collect data used by the bar and set them into variables
+HEADPHONE=$(amixer controls | grep "name='Headphone Jack'" | cut -d ',' -f1 | cut -d '=' -f2)
 WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}')
 WIRED=$(ls /sys/class/net/ | grep ^en | awk 'NR==1{print $1}')
 INTERFACE=$WIRED
@@ -16,5 +17,5 @@ tail -n+3 /proc/net/wireless | grep -q . && INTERFACE=$WIRELESS
 
 # Launch bar1 and bar2
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-	INTERFACE_NAME=$INTERFACE WIRELESS_INTERFACE=$WIRELESS WIRED_INTERFACE=$WIRED MONITOR=$m polybar --reload mainbar-herbst &
+	HEADPHONE_ID=$HEADPHONE INTERFACE_NAME=$INTERFACE WIRELESS_INTERFACE=$WIRELESS WIRED_INTERFACE=$WIRED MONITOR=$m polybar --reload mainbar-herbst &
 done
