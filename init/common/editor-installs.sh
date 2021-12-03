7207d99b6470f91989daaa78cf056a748f6569d9
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-12-03 17:05:17 +0100

    Add phpactor install step

--- init/common/editor-installs.sh
+++ init/common/editor-installs.sh
@@ -1,5 +1,10 @@
 # Installs the plugins / modules for the editors
 
+# Installs phpactor as a php lsp
+if [ -x "$(command -v composer)" ] && [ -f "$HOME/.config/composer/composer.json" ] ; then
+    composer install --working-dir "$HOME/.config/composer"
+    sudo ln -s "$HOME/.config/composer/vendor/bin/phpactor" /usr/local/bin/phpactor
+fi
 if [ -d "$HOME/.config/doom" ] && [ -f "$HOME/.config/emacs/bin/doom" ] ; then
     "$HOME/.config/emacs/bin/doom" sync
 fi
