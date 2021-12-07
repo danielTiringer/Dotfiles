df2f932849961e06248ecd290008d5432309f10c
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-12-07 08:11:51 +0100

    Install jdtls

--- init/common/jdtls.sh
+++ init/common/jdtls.sh
@@ -1,4 +1,3 @@
 # Installs eclipse's jdtls
 
-sudo mkdir -p /usr/share/java/jdtls
 curl --location https://download.eclipse.org/jdtls/milestones/1.6.0/jdt-language-server-1.6.0-202111261512.tar.gz | tar --extract --gzip --verbose --directory /home/daniel/.local/lib/jdtls
