dde983844badf63101b994504a651f5293cb6834
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-12-09 10:29:18 +0100

    Add fedora to update

--- zsh/.config/zsh/aliasfunctions.sh
+++ zsh/.config/zsh/aliasfunctions.sh
@@ -45,6 +45,7 @@ distro_update () {
     alpine) sudo apk -U upgrade                                               ;;
     arch)   sudo pacman -Syuu --noconfirm                                     ;;
     debian) sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y  ;;
+    fedora) sudo dnf check-update && sudo dnf upgrade                         ;;
     *)      echo -n "This distro is not set up in the script."                ;;
   esac
 }
