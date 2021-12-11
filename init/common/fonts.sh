1d1976a50a7915cd8ef79d0c5b3fce510c236860
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-12-11 13:41:37 +0100

    Make font install posix compliant

--- init/common/fonts.sh
+++ init/common/fonts.sh
@@ -1,21 +1,21 @@
 # Installs fonts from non-distro-specific sources
 
 # Nerd fonts
-NERD_FONTS=(
-    "DejaVuSansMono"
-    "JetBrainsMono"
-    "Mononoki"
-    "UbuntuMono"
-)
-NERD_FONT_RELEASE="v2.1.0"
-
 mkdir -p $HOME/.local/share/fonts
 
-for FONT in "${NERD_FONTS[@]}"; do
+NERD_FONTS="DejaVuSansMono JetBrainsMono Mononoki UbuntuMono"
+NERD_FONT_RELEASE="v2.1.0"
+
+set -f; IFS=' '
+
+for FONT in $NERD_FONTS; do
+    set +f; unset IFS
     curl --fail --silent --show-error --location "https://github.com/ryanoasis/nerd-fonts/releases/download/${NERD_FONT_RELEASE}/${FONT}.zip" --output "$HOME/Downloads/${FONT}.zip"
     unzip "$HOME/Downloads/${FONT}".zip -d $HOME/.local/share/fonts/
     rm -rf "$HOME/Downloads/${FONT}.zip"
 done
+
+set +f; unset IFS
 
 # Powerline Fonts
 git clone https://github.com/powerline/fonts.git --depth=1 $HOME/Downloads/fonts
