8e8c3709fd879b2836e7069635f4efa571316259
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-12-12 11:41:02 +0100

    Specify the installed alacritty version

--- init/common/alacritty.sh
+++ init/common/alacritty.sh
@@ -5,12 +5,15 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 rustup override set stable
 rustup update stable
 
-git clone https://github.com/alacritty/alacritty.git ~/Downloads/alacritty
-cd ~/Downloads/alacritty
+ALACRITTY_VERSION=0.9.0
+
+git clone https://github.com/alacritty/alacritty.git $HOME/Downloads/alacritty
+cd $HOME/Downloads/alacritty
+git checkout "$ALACRITTY_VERSION"
 
 cargo build --release
 
-sudo mv ~/Downloads/alacritty/target/release/alacritty /usr/local/bin/
+sudo mv $HOME/Downloads/alacritty/target/release/alacritty /usr/local/bin/
 
-cd ~
-rm -rf ~/Downloads/alacritty
+cd
+rm -rf $HOME/Downloads/alacritty
