e4bf338380e5690569c14f28361ddccfdfa7de0f
Daniel Tiringer <daniel@T430>
2021-12-12 19:21:31 +0100

    Correct the version number

--- init/common/alacritty.sh
+++ init/common/alacritty.sh
@@ -5,7 +5,7 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 rustup override set stable
 rustup update stable
 
-ALACRITTY_VERSION=0.9.0
+ALACRITTY_VERSION=v0.9.0
 
 git clone https://github.com/alacritty/alacritty.git $HOME/Downloads/alacritty
 cd $HOME/Downloads/alacritty
