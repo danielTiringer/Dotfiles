23914e38569b5005de2651fdcc367f8424996a09
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-12-12 21:38:03 +0100

    Add literal % sign

--- polybar/.config/polybar/scripts/cpu.sh
+++ polybar/.config/polybar/scripts/cpu.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+TEMPERATURE=$(sensors | grep "Package id 0:" | tr -d '+' | awk '{print $4}')
+
 echo " $(grep 'cpu ' /proc/stat | awk '{cpu_usage=($2+$4)*100/($2+$4+$5)}
-END {printf "%0.2f%", cpu_usage}')  $(sensors | grep temp1 | head -1 | awk '{print $2}')"
+END {printf "%0.2f%%", cpu_usage}')  $(sensors | grep temp1 | head -1 | awk '{print $2}')"
