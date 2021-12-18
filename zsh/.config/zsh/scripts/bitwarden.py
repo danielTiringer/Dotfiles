63d582f9a3070cd6fdbd79074a559d8457ba93f5
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-12-18 21:39:29 +0100

    Add option selections

--- zsh/.config/zsh/scripts/bitwarden.py
+++ zsh/.config/zsh/scripts/bitwarden.py
@@ -6,6 +6,17 @@ import re
 import subprocess
 
 class Bitwarden:
+    main_menu_options = {
+        1: 'Create new item',
+        2: 'Cancel',
+    }
+    item_types = {
+        1: 'Login',
+        2: 'Secure Note',
+        3: 'Card',
+        4: 'Identity',
+        5: 'Cancel',
+    }
     session_key = ''
     folders = []
 
@@ -13,6 +24,23 @@ class Bitwarden:
         self.check_bitwarden_login()
         self.unlock_vault()
         self.sync_vault()
+
+    def process_request(self):
+        while(True):
+            print('What would you like to do?')
+            self.print_menu(self.main_menu_options)
+            option = ''
+            try:
+                option = int(input('Enter your choice: '))
+            except:
+                print('Wrong input. Please enter a number...')
+
+            if option == 1:
+                self.process_new_item_request()
+
+            if option == 2:
+                print('User aborted.')
+                exit(0)
 
     def check_bitwarden_login(self):
         status_string = subprocess.getoutput('bw status')
@@ -22,7 +50,6 @@ class Bitwarden:
             exit(1)
 
     def unlock_vault(self):
-        print('First we need to log into Bitwarden...')
         password = self.get_users_password()
         status = subprocess.getoutput(f"echo {password} | bw unlock")
         substring = re.search(r'"([A-Za-z0-9/+=]*)=="', status)
@@ -32,16 +59,30 @@ class Bitwarden:
         return getpass('Please enter your Bitwarden password: ')
 
     def sync_vault(self):
-        print('We have to update the vault, standby...')
         status = subprocess.getstatusoutput(f"bw sync --session {self.session_key}")
         if status[0] != 0:
             print('Unable to sync the vault. Please try again later.')
             exit(1)
 
-        print('Vault update complete.')
+    def process_new_item_request(self):
+        while(True):
+            print('Please select which kind of item you want to create:')
+            self.print_menu(self.item_types)
+            option = ''
+            try:
+                option = int(input('Enter your choice: '))
+            except:
+                print('Wrong input. Please enter a number...')
+
+            if option == 5:
+                print('User aborted.')
+                exit(0)
+
+    def print_menu(self, list):
+        for key in list.keys():
+            print(key, '--', list[key])
 
     def get_folders(self):
-        print('Getting a list of folders...')
         folders_string = subprocess.getoutput(f"bw list folders --session {self.session_key}")
         folders_json = json.loads(folders_string)
         folders = [a_dict['name'] for a_dict in folders_json]
@@ -49,8 +90,7 @@ class Bitwarden:
 
 def main():
     bw = Bitwarden()
-    bw.get_folders()
-    print(bw.folders)
+    bw.process_request()
 
 if __name__ == '__main__':
     main()
