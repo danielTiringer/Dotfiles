485cd68257638b7bbc26f414ffbbc8fedcb8741b
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-12-18 05:23:10 +0100

    Limit the amount of installed languages

--- nvim/.config/nvim/plug-config/treesitter.lua
+++ nvim/.config/nvim/plug-config/treesitter.lua
@@ -1,7 +1,23 @@
 local ts = require'nvim-treesitter.configs'
 
 ts.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = {
+      "bash",
+      "html",
+      "java",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "php",
+      "python",
+      "ruby",
+      "tsx",
+      "typescript",
+      "vim",
+      "vue",
+      "yaml",
+  }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
   },
