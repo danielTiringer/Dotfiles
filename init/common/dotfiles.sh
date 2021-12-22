3b2f1e1a7c1572399b493c86a8d8d46fc38137a1
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-12-22 19:24:54 +0100

    Clone bitwarden wrapper

--- init/common/dotfiles.sh
+++ init/common/dotfiles.sh
@@ -3,3 +3,6 @@
 
 cd "$DOTFILEDIR"
 ./stowrestore
+
+# Clone standalone repository that is used by zsh
+git clone git@github.com:danielTiringer/Bitwarden-Wrapper.git $HOME/.config/zsh/scripts/Bitwarden-Wrapper
