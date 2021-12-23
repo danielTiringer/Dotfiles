ce4563516d3b0187cd9678d44e7bd28af9c2a989
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-12-23 17:56:51 +0100

    Clean up the installer script

--- init/debian.sh
+++ init/debian.sh
@@ -66,12 +66,12 @@ sudo apt install -yy vifm
 sudo apt install -yy zsh
 . "${INITDIR}/common/zsh.sh"
 
+# Install the xorg graphical environment
+sudo apt install -yy xorg
+
 # Install fonts
 sudo apt install -yy fonts-font-awesome
 . "${INITDIR}/common/fonts.sh"
-
-# Install the xorg graphical environment
-sudo apt install -yy xorg
 
 # Install window manager basics
 sudo apt install -yy nitrogen picom dmenu polybar xtrlock i3lock feh sxhkd lm-sensors rofi
@@ -110,76 +110,16 @@ sudo apt install -yy silversearcher-ag fzf gripgrep fd-find
 sudo apt install -yy -t unstable neovim
 . "${INITDIR}/common/neovim.sh"
 
-# Install emacs module dependencies
-# sudo apt install -yy shellcheck # for the sh lang
-# sudo apt install -yy markdown # for the markdown lang
-# sudo apt install -yy sbcl # for the common-lisp lang
-# sudo apt install -yy maim # for the org-mode module
-# sudo apt install -yy editorconfig # for editorconfig
-# pip3 install isort pipenv pytest nose python-lsp-server # for the python lang
-# sudo apt install -yy php-cli php-zip php-curl php-mbstring php-xml # for the php lang
-# curl --fail --silent --show-error --location https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer # also for php
-# sudo apt install -yy nodejs npm # for the web lang and vue
-# sudo apt install -yy node-js-beautify tidy && sudo npm install --global stylelint # for the web lang
-# sudo npm install --global vue-language-server # for vue
-# sudo apt install -yy ripgrep fd-find
-
-# Install emacs itself - from source, as the Debian library is too old for doom
-# EMACS_VERSION=emacs-27.2
-# sudo apt install -y \
-#     build-essential \
-#     texinfo \
-#     libx11-dev \
-#     libxpm-dev \
-#     libjpeg-dev \
-#     libpng-dev \
-#     libgif-dev \
-#     libtiff-dev \
-#     libgtk2.0-dev \
-#     libncurses-dev \
-#     libxpm-dev \
-#     automake \
-#     autoconf \
-#     libgnutls28-dev \
-#     libjansson-dev \
-#     libxml2-dev
-# curl --location https://quantum-mirror.hu/mirrors/pub/gnu/emacs/${EMACS_VERSION}.tar.gz --output ~/Downloads/${EMACS_VERSION}.tar.gz
-# cd ~/Downloads
-# tar -xvzf ~/Downloads/${EMACS_VERSION}.tar.gz
-# rm ~/Downloads/${EMACS_VERSION}.tar.gz
-# cd ~/Downloads/${EMACS_VERSION}
-# ./configure --with-json
-# make
-# sudo make install
-# cd ~
-# rm -rf ~/Downloads/${EMACS_VERSION}
-
-# Install doom
-# . "${INITDIR}/common/doom.sh"
-# If the command below doesn't work, run Alt-X all-the-icons-install-fonts
-# emacs --batch -f all-the-icons-install-fonts
-# According to henrik, the above runs emacs without doom, so it doesn't know what all-the-icons are. Hopefully this will work:
-# emacs --eval '(all-the-icons-install-fonts t)'
-
 # Install API tester
 sudo apt install -yy httpie
 # . "${INITDIR}/common/postman.sh"
-
-# Install image manipulation program
-sudo apt install -yy imagemagick #gimp
 
 # Set up firewall
 sudo apt install -yy ufw
 sudo systemctl enable ufw.service --now
 . "${INITDIR}/common/ufw.sh"
 
-# Install browser
-sudo apt update -yy && sudo apt install -yy -t unstable firefox
-
-# Install multimedia
-sudo apt install -yy  alsa-utils pulsemixer mpv
-
-# Install Docker and Docker-Compose
+# Install docker and docker-compose
 sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl --fail --silent --show-error --location https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 echo "deb http://download.docker.com/linux/debian $(lsb_release --codename --short) stable" | sudo tee /etc/apt/sources.list.d/docker.list
@@ -187,27 +127,14 @@ sudo apt update -yy
 sudo apt install -yy docker-ce docker-ce-cli containerd.io
 
 . "${INITDIR}/common/docker-compose.sh"
-
 sudo groupadd docker
 sudo usermod -aG docker "$USER"
 
-# Install Virtualbox
-# curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor -o /usr/share/keyrings/virtualbox-keyring.gpg
-# echo "deb [arch=amd64 signed-by=/usr/share/keyrings/virtualbox-keyring.gpg] https://download.virtualbox.org/virtualbox/debian bullseye contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-# sudo apt update && sudo apt install -y virtualbox-6.1
+# Install browser
+sudo apt update -yy && sudo apt install -yy -t unstable firefox
 
-# Install Virt-Manager
-# sudo apt-get install -y qemu qemu-kvm qemu-system qemu-utils
-# sudo apt-get install -y virtinst libvirt-clients libvirt-daemon-system
-# sudo systemctl start libvirtd
-# sudo virsh net-start default
-# sudo virsh net-autostart default
-# sudo apt-get install -y virt-manager
-# sudo usermod -G libvirt -a $USER
-# sudo /etc/init.d/networking restart
-
-# Install Dosbox
-sudo apt install -y dosbox
+# Install multimedia
+sudo apt install -yy  alsa-utils pulsemixer mpv
 
 # Install password manager
 . "${INITDIR}/common/bitwarden.sh"
@@ -227,12 +154,6 @@ if [ "$(hardware_type)" = 'MacBook' ] ; then
 	. "${INITDIR}/specific/macbook-fan.sh"
 	. "${INITDIR}/specific/macbook-keyboard-brightness.sh"
 fi
-
-# https://www.youtube.com/watch?v=EzqgJhu-qN8
-echo '
-# Samba fileshare of synology nas
-# //192.168.10.49/Media  /media/smb/  cifs  guest,iocharset=utf8,file_mode=0777,dir_mode=0777,credentials=/home/daniel/.config/samba/credentials
-' | sudo tee -a /etc/fstab
 
 # Install complete
 . "${INITDIR}/common/restart.sh"
