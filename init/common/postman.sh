3ec228092f846ccb5f95a05309c8cd911049009c
Daniel Tiringer <tiringerdaniel@tutanota.com>
2022-01-05 08:14:37 +0100

    Update to use variables and long arguments

--- init/common/postman.sh
+++ init/common/postman.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Installs postman
 
-curl --location https://dl.pstmn.io/download/latest/linux64 --output ~/Downloads/postman-linux-x64.tar.gz
-sudo tar -xvzf ~/Downloads/postman-linux-x64.tar.gz -C /opt
+curl --location https://dl.pstmn.io/download/latest/linux64 --output $HOME/Downloads/postman-linux-x64.tar.gz
+sudo tar --extract --verbose --gzip --file $HOME/Downloads/postman-linux-x64.tar.gz --directory /opt
 sudo ln -s /opt/Postman/Postman /usr/bin/postman
 rm ~/Downloads/postman-linux-x64.tar.gz
