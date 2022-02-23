056fdfd559416acd99914dc25a56e3460166f5e6
Daniel Tiringer <tiringerdaniel@tutanota.com>
2022-02-23 12:00:33 +0100

    Install jetbrains ide-s on debian

--- init/debian.sh
+++ init/debian.sh
@@ -110,6 +110,20 @@ install silversearcher-ag fzf gripgrep fd-find
 install -t unstable neovim
 . "$INITDIR/common/neovim.sh"
 
+# Enable the jetbrains repository
+curl -s https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | gpg --dearmor | sudo tee /usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg] http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com focal main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
+sudo apt update
+
+# Install phpstorm
+install phpstorm
+
+# Install intellij
+install intellij-idea-community
+
+# Install webstorm
+install webstorm
+
 # Install API tester
 install httpie
 # . "$INITDIR/common/postman.sh"
