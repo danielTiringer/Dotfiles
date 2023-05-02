5f36ce9f2ca1397293c858780feb28020f6de1c4
Daniel Tiringer <daniel@Daniels-MBP.chello.hu>
2023-05-02 19:43:00 +0200

    Start on mac script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,26 @@
 
 # Entrypoint for the automated system installer
 
-DOTFILEDIR="$(dirname "$(realpath "$0")")"
+if [[ "$OSTYPE" == "linux-gnu"* ]] ; then
+  DOTFILEDIR="$(dirname "$(realpath "$0")")"
 
-INITDIR="$DOTFILEDIR/init"
-. "$INITDIR/helper.sh"
+  INITDIR="$DOTFILEDIR/init"
+  . "$INITDIR/helper.sh"
 
-DISTRO=$(check_distro)
-DISTROSCRIPT="$INITDIR/$DISTRO.sh"
+  DISTRO=$(check_distro)
+  DISTROSCRIPT="$INITDIR/$DISTRO.sh"
 
-if [ ! -f "$DISTROSCRIPT" ] ; then
-  echo "No install script was found for $DISTRO."
+  if [ ! -f "$DISTROSCRIPT" ] ; then
+    echo "No install script was found for $DISTRO."
+    exit 1
+  fi
+
+  . "$DISTROSCRIPT"
+elif [[ "$OSTYPE" == "darwin"* ]] ; then
+  DOTFILEDIR=$(cd "$(dirname "$0")"; pwd -P)
+  INITDIR="$DOTFILEDIR/init"
+  . "$INITDIR/mac.sh"
+else
+  echo "No install script was found for the $OSTYPE."
   exit 1
 fi
-
-. "$DISTROSCRIPT"
---
+++ init/mac.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Start from a base MacOS (Catalina) system, with git installed (type `git` into the terminal and install the developer tools)
+
+# Install prompt
+echo 'The executed script will install applications on a MacOS based system.'
+
+# Install homebrew
+if test ! $(which brew); then
+  sudo git # Need to get into sudo for brew, but not run it with sudo... weird stuff
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew update
+fi
+
+# Install chrome
+if [ ! -x "/Applications/Google Chrome.app" ] ; then
+  brew install --cask google-chrome
+fi
+
+# Install firefox
+if [ ! -x "/Applications/Firefox.app" ] ; then
+  curl \
+    --location \
+    "https://download.mozilla.org/?product=firefox-latest-ssl&os=osx&lang=en-US" \
+     --output "$HOME"/Downloads/Firefox.dmg
+  hdiutil attach Firefox.dmg -nobrowse -readonly
+  cp -R /Volumes/Firefox/Firefox.app /Applications/
+  hdiutil detach /Volumes/Firefox/ -force
+  rm -r "$HOME"/Downloads/Firefox.dmg
+fi
