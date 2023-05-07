3065da0b331c97ea09dd3d2b4d5ec8718cfd8b7f
Daniel Tiringer <daniel@Daniels-MBP.chello.hu>
2023-05-08 00:19:31 +0200

    Renaming stow script

--- init/common/dotfiles.sh
+++ init/common/dotfiles.sh
@@ -2,7 +2,7 @@
 # Distributes dotfiles
 
 cd "$DOTFILEDIR"
-./stowrestore.sh
+./stow_linux.sh
 
 # Clone standalone repository that is used by zsh
 git clone git@github.com:danielTiringer/Bitwarden-Wrapper.git "$HOME"/.config/zsh/scripts/Bitwarden-Wrapper
