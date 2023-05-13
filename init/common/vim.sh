fd5dee3a0e84b14e94f56f994849e073f8ae7b24
Daniel Tiringer <tiringerdaniel@tutanota.com>
2023-05-13 17:41:05 +0200

    Handle bundle directory not exist case

--- init/common/vim.sh
+++ init/common/vim.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # Install the Vundle plugin manager
 
+if [ ! -x "$HOME"/.vim/bundle ] ; then
+	mkir "$HOME"/.vim/bundle
+fi
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
