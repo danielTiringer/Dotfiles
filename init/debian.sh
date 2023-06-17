217d70fa693fbd18e281e82df279ee8adb722025
Daniel Tiringer <tiringerdaniel@tutanota.com>
2023-06-17 18:15:16 +0200

    Update docker repo to match current auth methods

--- init/debian.sh
+++ init/debian.sh
@@ -136,8 +136,13 @@ enable_service ufw
 
 # Install docker and docker-compose
 install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl_default https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-echo "deb http://download.docker.com/linux/debian $(lsb_release --codename --short) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo install -m 0755 -d /etc/apt/keyrings
+curl_default https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update -yy
 install docker-ce docker-ce-cli containerd.io
 
