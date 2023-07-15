9db7e3ac18bbcd155b47ea7426883945f52aba14
Daniel Tiringer <tiringerdaniel@tutanota.com>
2023-07-15 09:49:49 +0200

    Install vim

--- init/debian.sh
+++ init/debian.sh
@@ -86,6 +86,10 @@ if ! is_wayland ; then
 
 fi
 
+# Install vim
+install vim
+. "$INITDIR/common/vim.sh"
+
 # Enable the jetbrains repository
 curl_default https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | gpg --dearmor | sudo tee /usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg] http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com focal main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
