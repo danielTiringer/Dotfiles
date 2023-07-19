0b3ad6d765e8a58050a1d63bcce6f6efcd92f726
Daniel Tiringer <tiringerdaniel@tutanota.com>
2023-07-19 08:58:58 +0200

    Silence any messages

--- init/common/folders.sh
+++ init/common/folders.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Creates basic folder structure
 
-mkdir ~/Downloads ~/Pictures ~/Documents ~/Projects ~/.config
+mkdir ~/Downloads ~/Pictures ~/Documents ~/Projects ~/.config > /dev/null 2>&1
 sudo mkdir -p /media/samba /media/MemCard /media/USB
