f09ed9f2d2217e77078a5ab331673b3bed1fd1e0
Daniel Tiringer <tiringerdaniel@tutanota.com>
2023-12-26 21:50:28 +0100

    Modify jetbrains ppa to include latest

--- init/debian.sh
+++ init/debian.sh
@@ -92,7 +92,7 @@ install vim
 
 # Enable the jetbrains repository
 curl_default https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | gpg --dearmor | sudo tee /usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg] http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com focal main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg] http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com any main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
 sudo apt update -yy
 
 # Configure file watchers
