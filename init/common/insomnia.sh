5e1cd72e2900a8eea8d5dab1c88921f0e4791404
Daniel Tiringer <tiringerdaniel@tutanota.com>
2024-03-29 09:59:32 +0100

    Update latest insomnia method

--- init/common/insomnia.sh
+++ init/common/insomnia.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-LATEST_INSOMNIA_VERSION=$(curl --silent https://api.github.com/repos/kong/insomnia/releases/latest | jq '.tag_name' | tr -d '"')
+LATEST_INSOMNIA_VERSION=$(curl --silent https://api.github.com/repos/kong/insomnia/releases/latest | jq '.tag_name' | tr -d '"' | sed 's/lib@//')
 LATEST_INSOMNIA_FILENAME=$(curl --silent https://api.github.com/repos/kong/insomnia/releases/latest | jq '.assets[] | select(.name | strings | test(".tar.gz")) | .name' | tr -d '"')
 INSOMNIA_URI="https://github.com/Kong/insomnia/releases/download/$LATEST_INSOMNIA_VERSION/$LATEST_INSOMNIA_FILENAME"
 echo $INSOMNIA_URI
